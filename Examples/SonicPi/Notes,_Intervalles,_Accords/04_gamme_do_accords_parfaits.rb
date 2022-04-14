491f477352772b5d7ec230756bfdc1aec4c7ccf7
Jean-Baptiste Lievremont <mithfindel@gmail.com>
2022-04-14 19:09:45 +0200

    Add Sonic Pi samples of playing notes, intervals, chords

---
+++ Examples/SonicPi/Notes,_Intervalles,_Accords/04_gamme_do_accords_parfaits.rb
@@ -0,0 +1,35 @@
+#
+# De plus en plus fort : on rajoute la quinte (accords "parfaits")
+#
+
+play :C3
+play :E3    #                              G4
+play :G3    #--------------------------F4-----
+demi_temps  #                      E4      E4
+play :D3    #------------------D4------D4-----
+play :F3    #              C4      C4      C4
+play :A3    #----------B3------B3------B3-----
+demi_temps  #      A3      A3      A3
+play :E3    #--G3------G3------G3-------------
+play :G3    #      F3      F3
+play :B3    #--E3------E3---------------------
+demi_temps  #      D3
+play :F3    # -C3-
+play :A3    #
+play :C4
+demi_temps
+play :G3
+play :B3
+play :D4
+demi_temps
+play :A3
+play :C4
+play :E4
+demi_temps
+play :B3
+play :D4
+play :F4
+demi_temps
+play :C4
+play :E4
+play :G4
